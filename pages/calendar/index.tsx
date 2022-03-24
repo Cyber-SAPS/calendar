8c175ec02342353b1e7beaad597e7d7842feceaf
Cyber-SAPS <cybersaps@gmail.com>
2022-03-24 09:06:20 -0400

    Integrated tailwind css into application

--- pages/calendar/index.tsx
+++ pages/calendar/index.tsx
@@ -7,20 +7,21 @@ import eventsData from "@/features/addons/events.json"
 export default function EventCalendar(){
     const [events] = React.useState(eventsData)
 
-
     if(typeof events[0] === "undefined") return <>Coming Soon</>;
     if(events.length <= 0) return <>Coming Soon</>;
 
     return(
-        <div>
+        <div className='border'>
             <h2 className="text-center">Calendar of Events</h2>
             <div className="grid grid-cols-8 gap-4">
-                <div className="col-span-2 p-10 flex flex-col">            
+                <div className="col-span-2 p-10 flex flex-col">   
+                Left Side Content         
                 </div>
-                <div className="col-span-6 p-5">
+                <div className="col-span-6 p-5 border border-dotted border-black">
                 <Calendar events={events} />
                 </div>
             </div>
         </div>
     )
 }
+
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,19 @@
+import Calendar from './calendar'
 import type { NextPage } from 'next'
 import * as React from 'react'
+import Head from 'next/head'
 
 
 const Home: NextPage = () => {
   return (
-   <React.Fragment>
-     Home Page
-
+    <React.Fragment>
+    <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=0.86, maximum-scale=5.0, minimum-scale=0.86"/>
+    </Head>
+   <div className="container md:mx-auto">
+     {/* Home Page */}
+     <Calendar />
+   </div>
    </React.Fragment>
   )
 }
